6eb93ec9e75aa9160de0c670629b879330ed906d
Luis Sanchez <luisanchezdh@gmail.com>
2022-11-17 15:01:16 -0500

    finished readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# command-line-app
+### Contact List Terminal Application via Python/SQL
+
+## Create and read Contacts 
+
+## How to use:
+# Fork and clone this repo
+# Create a contacts database
+# Edit the seed file to the contacts and its addresses to your liking
+# Run both schema.sql and seed.sql files: 
+```bash
+psql -d contacts < seed.sql
+psql -d contacts < schema.sql
+```
--- seed.sql
+++ seed.sql
@@ -8,8 +8,8 @@ INSERT INTO addressesinfo (street_name,city,zipcode) VALUES ('South Bronx','Bron
 
 
 --Contacts
-INSERT INTO contacts(first_name, last_name,phone_number,address_id) VALUES ('Nasir', 'Jones', 91794,1);
-INSERT INTO contacts(first_name, last_name, phone_number, address_id) VALUES ('Big','L',91433,2);
-INSERT INTO contacts(first_name, last_name, phone_number, address_id) VALUES ('Luis', 'Sanchez', 91344, 3);
-INSERT INTO contacts(first_name, last_name, phone_number, address_id) VALUES ('Naruto','Uzumaki', 646204, 4);
-INSERT INTO contacts(first_name, last_name, phone_number, address_id) VALUES ('Kay', 'Flock', 9179998, 5);
+INSERT INTO contactsinfo(first_name, last_name,phone_number,address_id) VALUES ('Nasir', 'Jones', 91794,1);
+INSERT INTO contactsinfo(first_name, last_name, phone_number, address_id) VALUES ('Big','L',91433,2);
+INSERT INTO contactsinfo(first_name, last_name, phone_number, address_id) VALUES ('Luis', 'Sanchez', 91344, 3);
+INSERT INTO contactsinfo(first_name, last_name, phone_number, address_id) VALUES ('Naruto','Uzumaki', 646204, 4);
+INSERT INTO contactsinfo(first_name, last_name, phone_number, address_id) VALUES ('Kay', 'Flock', 9179998, 5);
